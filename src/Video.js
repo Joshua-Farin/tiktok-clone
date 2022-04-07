b6bd5d1a0722ac8c84f1d8bd185c203cc41a6bd7
Joshua Farin <joshua.k.farin@gmail.com>
2022-04-07 08:39:11 -1000

    Add deps & videoFooter

--- src/Video.js
+++ src/Video.js
@@ -1,5 +1,7 @@
 import React, { useRef, useState } from 'react'
 import './Video.css';
+import VideoFooter from './VideoFooter';
+ 
 
 function Video() {
     const [playing, setPlaying] = useState(false);
@@ -12,9 +14,8 @@ function Video() {
         } else {
             videoRef.current.play();
             setPlaying(true);
-        }
-        
-    }
+        }  
+    };
   return (
     <div className='video'>
       <video
@@ -23,7 +24,8 @@ function Video() {
       loop
       ref={videoRef}
       src='https://v16m-webapp.tiktokcdn-us.com/a6d68aa89879ef0c821a97297a7e82d7/624f5fe5/video/tos/useast5/tos-useast5-ve-0068c002-tx/3b0e4f02f6374839bea80db0a62cafaa/?a=1988&br=6044&bt=3022&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XY53A3E7nz7Th8z3SDXq&l=2022040716041601011313507813013FF9&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anZ2Omk6ZjpoPDMzZzczNEApNzo0ODZmOzs4N2dpMzo8aGdzZDZicjRnXjJgLS1kMS9zcy02Ml8zYzFhYjBgMl42MF46Yw%3D%3D&vl=&vr='></video>
-
+    
+    <VideoFooter />
       {/* {video footer} */}
       {/* {video sidebar} */}
     </div>
--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import './VideoFooter.css';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import Ticker from "react-ticker";
 
 function VideoFooter() {
   return (
     <div className='videoFooter'>
-      
+      <div className='videoFooter__text'>
+        <h3>@Ssdfsdfsdf</h3>
+        <p>Description</p>
+        <div className='videoFooter__ticker'>
+          <MusicNoteIcon className='videoFooter__icon' />
+
+          <Ticker mode ='smooth'>
+            {({ index }) => (
+                <>
+                    <p>this is song</p>
+                </>
+            )}
+          </Ticker>
+        </div>
+      </div>
+      <img
+        className='videoFooter__record'
+        src='https://static.thenounproject.com/png/934821-200.png'
+      />
     </div>
   )
 }
