14a76dd260bc68e7a05cbf90e3486ec751b0f58e
Joshua Farin <joshua.k.farin@gmail.com>
2022-04-07 06:59:28 -1000

    Add functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import React from 'react';
 import './App.css';
+import Video from './Video';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Build a tiktok clone with merns tack</h1>
+    <div className="app">
+      <div className='app__videos'>
+        <Video />
+        <Video />
+      </div>
+      
+      {/* {app container} */}
+      {/* {videos} */}
+      {/* {} */}
     </div>
   );
 }
