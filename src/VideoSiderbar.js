b3fd2810068ceae77e4f32175ffbd4bfe9458a8b
Joshua Farin <joshua.k.farin@gmail.com>
2022-04-07 07:55:02 -1000

    Add footer & sidebar

---
+++ src/VideoSiderbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './VideoSidebar.css';
+
+function VideoSiderbar() {
+  return (
+    <div className='videoSiderbar'>
+      
+    </div>
+  )
+}
+
+export default VideoSiderbar
